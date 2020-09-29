482ea1a43ebf4fff06b6b800056f848cf50a3c91
dfoxpro <dfoxpro@gmail.com>
2020-09-29 18:59:02 -0500

    pegasus & GBA working config

---
+++ firmware/preinstall/external/Thumbnails/Nintendo - Game Boy Advance/makeLinksToSkraperMediaFolders.sh
@@ -0,0 +1,3 @@
+ln -s /storage/sdcard1/Roms/GBA/media/screenshottitle Named_Titles
+ln -s /storage/sdcard1/Roms/GBA/media/screenshot Named_Snaps
+ln -s /storage/sdcard1/Roms/GBA/media/box2dfront Named_Boxarts
